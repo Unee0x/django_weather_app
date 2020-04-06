4a57b70d3a246210fd11e1123fb7b1e29b7c1ffb
Unee0x <Unee0x@gmail.com>
2020-04-06 19:38:08 -0400

    file change

--- django_weather_app/the_weather/weather/views.py
+++ django_weather_app/the_weather/weather/views.py
@@ -7,9 +7,39 @@ def index(request):
     
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=c05f9a60c3350fc7044c8d73958bd224'
 
+    err_msg = ''
+
+    message = ''
+
+    message_class = ''
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist!'
+
+            else:
+                err_msg = 'City already exists in the database!'
+
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+
+        else:
+            message = 'City added successfully!'
+            message_class = 'is-success'
+
+    print(err_msg)
 
     form = CityForm()
 
@@ -30,6 +60,11 @@ def index(request):
     print( weather_data )
 
     print(city_weather)
-    context = { 'weather_data' : weather_data, 'form': form }
+    context = {
+        'weather_data' : weather_data,
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class
+    }
     print(r)
     return render(request, 'weather/weather.html', context)
